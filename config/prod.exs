63e9e7d0987bfff1edee65e6efe33e9160f0c8d7
Alex Griffith <alexalexgriffith@gmail.com>
2019-02-24 14:28:09 -0500

    Add missing adapter config

--- config/prod.exs
+++ config/prod.exs
@@ -26,6 +26,7 @@ config :logger, level: :info
 
 # Configure your database
 config :swbatnet, Swbatnet.Repo,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
